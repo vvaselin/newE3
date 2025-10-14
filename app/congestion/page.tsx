42ca0f44b7468af560f71ed61cab70623c3a6a41
25NM775L <25nm775l@vc.ibaraki.ac.jp>
2025-10-14 15:58:16 +0900

    form

--- app/congestion/page.tsx
+++ app/congestion/page.tsx
@@ -1,13 +1,12 @@
 import type { Metadata } from "next";
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
 
 // 席数の指定
 const ROWS = 20;
 const COLS = 10;
-const COL_LABELS = Array.from({ length: COLS }, (_, i) => String.fromCharCode(65 + i));
 
 // 数字と色の対応
-const COLOR_MAP = {
+const COLOR_STATUS = {
   0: 'white', // 空席
   1: 'green', // 10分経過
   2: 'blue', // 20分経過
@@ -23,7 +22,38 @@ export default function congestion() {
   return (
     <Box>
       <Heading>混雑状況の確認</Heading>
-      
+      <Box marginTop={4}>
+        <Text display="inline">列</Text>
+        <select>
+          {Array.from({ length: COLS }, (_, i) => {
+            const letter = String.fromCharCode(65 + i);
+            return (
+              <option key={letter} value={letter}>
+                {letter}
+              </option>
+            );
+          })}
+        </select>
+        <Text display="inline">行</Text>
+        <select style={{marginLeft: "8px"}}>
+          {Array.from({ length: ROWS }, (_, i) => {
+            const number = i + 1;
+            return (
+              <option key={number} value={number}>
+                {number}
+              </option>
+            );
+          })}
+        </select>
+        <Text display="inline">色</Text>
+        <select style={{marginLeft: "8px"}}>
+          {Array.from({ length: 4 }, (_, i) => (
+            <option key={i} value={i}>
+              {i}
+            </option>
+          ))}
+        </select>
+      </Box>
     </Box>
   );
 }
