0dc77d87850ebbc2072324a2ca899db78d4d9d4f
25NM775L <25nm775l@vc.ibaraki.ac.jp>
2025-10-16 14:29:39 +0900

    test

--- app/congestion/page.tsx
+++ app/congestion/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Box, Heading, Text } from "@chakra-ui/react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 // 席数の指定
 const SEATS_NUM = 20;
@@ -16,7 +16,7 @@ const COLOR_STATUS = {
 
 export default function congestion() {
   const [SEATS, setSEATS] = useState<number[]>(Array.from({ length: SEATS_NUM }, () => 0));
-  const handleSubmit = () => {
+  const SeatRegister = () => {
     const seatInput = document.querySelector('input[name="seat"]') as HTMLInputElement;
     const seat = parseInt(seatInput.value) - 1;
     if (seat >= 0 && seat < SEATS_NUM) {
@@ -27,16 +27,44 @@ export default function congestion() {
       });
     }
   }
+  const SeatUnregister = () => {
+    const seatInput = document.querySelector('input[name="seat"]') as HTMLInputElement;
+    const seat = parseInt(seatInput.value) - 1;
+    if (seat >= 0 && seat < SEATS_NUM) {
+      setSEATS(prev => {
+        const newSeats = [...prev];
+        newSeats[seat] = 0;
+        return newSeats;
+      });
+    }
+  }
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSEATS(currentValue => currentValue.map(value => {
+        if (value >= 1){
+          return value + 1;
+        }
+        return value;
+        }));
+      }, 600000);
+    return () => clearInterval(interval);
+  }, []);
   return (
     <Box>
       <Heading>混雑状況の確認</Heading>
       <Box marginTop={4} display="flex" gap={4}>
+        <Text>席登録</Text>
         <input type="text" placeholder="席番号" name="seat" />
-        <button onClick={handleSubmit}>決定</button>
+        <button onClick={SeatRegister}>決定</button>
+      </Box>
+      <Box marginTop={4} display="flex" gap={4}>
+        <Text>席解除</Text>
+        <input type="text" placeholder="席番号" name="seat" />
+        <button onClick={SeatUnregister}>決定</button>
       </Box>
       <Box marginTop={4}>
         {SEATS.map((seat, index) => (
-          <Box key={index} width="40px" height="40px" border="1px solid black" backgroundColor={seat === 1 ? 'red' : COLOR_STATUS[seat as keyof typeof COLOR_STATUS]} margin="2px" />
+          <Box key={index} width="40px" height="40px" border="1px solid black" backgroundColor={COLOR_STATUS[seat as keyof typeof COLOR_STATUS]} margin="2px" />
         ))}
       </Box>
     </Box>
